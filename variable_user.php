56fee429e2615142bc60d09a895a26c5e7d99e65
Gondesanjay <doctor031102@gmail.com>
2025-02-20 18:39:54 +0700

    praktikum 1

---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendefinisikan variable
+$nama = 'Ikmal';
+$umur = 20;
+$berat = 54.4;
+
+echo 'Nama saya ' . $nama; 
+echo '<br/>Umur : ' . $umur . ' Tahun';
+echo '<br/>Berat : ' . $berat . ' Kg';
+
+?>
