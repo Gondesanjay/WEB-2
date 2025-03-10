b132641067a7caeeb7008623b09c6a90e654ca68
Gondesanjay <doctor031102@gmail.com>
2025-03-10 12:21:24 +0700

    Tugas Praktikum 3

---
+++ Praktikum-3/partials/sections/about.php
@@ -0,0 +1,24 @@
+<section class="page-section bg-primary text-white mb-0" id="about">
+            <div class="container">
+                <!-- About Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-white">About Me</h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom divider-light">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- About Section Content-->
+                <div class="row">
+                    <div class="col-lg-4 ms-auto"><p class="lead">Saya adalah seorang mahasiswa IT yang berfokus pada pengembangan keterampilan dalam bidang teknologi dan komputasi. Sebagai mahasiswa yang mempelajari berbagai disiplin ilmu terkait teknologi informasi, mereka dilatih untuk memahami cara kerja perangkat keras, perangkat lunak, serta cara mengintegrasikannya untuk menyelesaikan berbagai permasalahan teknis.</p></div>
+                    <div class="col-lg-4 me-auto"><p class="lead">Mahasiswa IT umumnya memiliki ketertarikan besar pada dunia teknologi, inovasi digital, dan pemecahan masalah menggunakan solusi berbasis teknologi. Mereka belajar berbagai bahasa pemrograman, analisis data, jaringan komputer, keamanan siber, serta pengembangan aplikasi dan perangkat lunak. Tidak hanya belajar tentang aspek teknis, mahasiswa.</p></div>
+                </div>
+                <!-- About Section Button-->
+                <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
+                        <i class="fas fa-download me-2"></i>
+                        Free Download!
+                    </a>
+                </div>
+            </div>
+        </section>
