641f4f556fba3ca35d5ca58a21d86285b9fdb56c
Gondesanjay <doctor031102@gmail.com>
2025-03-13 00:00:56 +0700

    Tugas Praktikum 3

---
+++ Task_Praktikum03/partials/cards/education-card-2.php
@@ -0,0 +1,20 @@
+<div class="card shadow border-0 rounded-4 mb-5">
+                                <div class="card-body p-5">
+                                    <div class="row align-items-center gx-5">
+                                        <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                            <div class="bg-light p-4 rounded-4">
+                                                <div class="text-secondary fw-bolder mb-2">2024</div>
+                                                <div class="mb-2">
+                                                    <div class="small fw-bolder">Backend Development</div>
+                                                    <div class="small text-muted">Node.js, Python, PHP, Ruby, Java</div>
+                                                </div>
+                                                <div class="fst-italic">
+                                                    <div class="small text-muted">Beginner</div>
+                                                    <div class="small text-muted">Backend Development</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-8"><div>Pengembangan sisi server menggunakan bahasa pemrograman yang memungkinkan pengelolaan data, otentikasi pengguna, dan pemrosesan logika aplikasi. Node.js memungkinkan pengembangan aplikasi JavaScript di sisi server, sementara bahasa lain seperti Python dan PHP juga sering digunakan dalam pengembangan backend.</div></div>
+                                    </div>
+                                </div>
+                            </div>
---
+++ Task_Praktikum03/partials/cards/project-card-2.php
@@ -0,0 +1,11 @@
+<div class="card overflow-hidden shadow rounded-4 border-0">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Traveler</h2>
+                                            <p>Tujuan utama dari penelitian ini adalah untuk mengeksplorasi dan menganalisis peran web travel dalam industri pariwisata modern. Untuk memberikan pemahaman yang lebih mendalam mengenai fungsi, fitur, dan manfaat yang ditawarkan oleh web travel bagi pengguna dan pelaku industri pariwisata, Untuk memahami kebutuhan dan preferensi pengguna dalam menggunakan web travel.</p>
+                                        </div>
+                                        <img class="img-fluid" src="assets/Project2.png" alt="..." style="width: 400px; height: 200px;" />
+                                    </div>
+                                </div>
+                            </div>
